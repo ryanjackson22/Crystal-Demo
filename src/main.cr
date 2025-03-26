e55f4ed54cc471e9a91a4ee122e87d1dad210d62
Ryan Jackson <ryan.jackson@mymail.champlain.edu>
2025-03-26 18:22:29 -0400

    implented bubble sort

--- src/main.cr
+++ src/main.cr
@@ -1,1 +1,18 @@
-puts "Hello World!"
+def sort_array(arr)
+    (0...arr.size - 1).each do |n|
+      swapped = false
+      (0...arr.size - n - 1).each do |i|
+        if arr[i] > arr[i + 1]
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+          swapped = true
+        end
+      end
+      break unless swapped
+    end
+    return arr
+  end
+
+
+ary = [4, 8, 10, 7, 2, 3, 9]
+puts ary.to_s
+puts sort_array(ary).to_s
